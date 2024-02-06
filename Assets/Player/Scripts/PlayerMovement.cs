0ae4bca521dcd29096c5db605efde72b5785bdf2
Gonzalo Martinez <ggmartinez.utu@gmail.com>
2024-02-06 17:48:03 -0300

    Migrated to New Input System

--- Assets/Player/Scripts/PlayerMovement.cs
+++ Assets/Player/Scripts/PlayerMovement.cs
@@ -1,20 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] float racketSpeed;
     [SerializeField] Rigidbody2D rigidBody;
+    private float movement;
 
     Vector2 racketDirection;
 
     void Update(){
-        float directionY = Input.GetAxis("Vertical");
-        racketDirection = new Vector2(0f, directionY).normalized;
+       racketDirection = new Vector2(0f, movement).normalized;
     }
 
     void FixedUpdate(){
-        rigidBody.velocity = racketDirection * racketSpeed;;
+        rigidBody.velocity = racketDirection * racketSpeed;
     }
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        movement = context.ReadValue<Vector2>().y;
+    }
+
 }
